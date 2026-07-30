32a80d36e52beecef581bc6b70338179f9af9118
tiyenn <s4165740@student.rmit.edu.au>
2026-07-30 12:17:32 +1000

    test note
    test button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.body.style.backgroundColor = "red" ;
+// document.body.style.backgroundColor = "red" ;
+// find my test button
+const testButton = document .getElementsByClassName("test-button");
+
+// create instrument and connect to audio
+const synth = new Tone.Synth() .toDestination(); 
+
+// do something when we click that button
+testButton .addEventListener ("click" , playTestNote);
+
+function playTestNote () {
+    synth .triggerAttackRelease("C4" , "8n")
+}
